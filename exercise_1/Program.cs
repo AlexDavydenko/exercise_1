0cd31f491f4e3ec7359073d65c1e9b2f9d975324
AlexDavydenko <57895837+AlexDavydenko@users.noreply.github.com>
2019-12-14 11:33:06 +0200

    Добавьте файлы проекта.

---
+++ exercise_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 15;
+            if (b < a)
+            {
+                Console.WriteLine(a + " больше " + b);
+            }
+            else
+            {
+                Console.WriteLine(b + " больше " + a);
+            }
+        }
+    }
+}
